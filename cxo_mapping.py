8a448f2a815104384077c20fc8070e4b714cc5f4
fk20268 <fk20268@bristol.ac.uk>
2022-09-14 10:20:08 +0100

    slight improvement

--- cxo_mapping.py
+++ cxo_mapping.py
@@ -8,20 +8,23 @@ accepted_words =['chief executive officer', 'chief underwriting officer', 'chief
 'head of claims','chief of claims', 'claims head', 'senior claims officer', 'chief actuary','chief actuarial',
 'chief digital transformation officer','chief digital','digital','head of counsel','general counsel',
 'head of data', 'chief internal auditor', 'chief strategy and innovation officer', 'chief audit executive',
- 'chief human resources officer', 'chief administrative officer', 'chief claims officer', 'chief communications officer']
+ 'chief human resources officer', 'chief administrative officer', 'chief claims officer', 'chief communications officer',
+ 'chief sustainability officer', 'chief finance officer', 'finance director']
 
 accepted_acronyms = ['CEO', 'CUO', 'CFO', 'CTO', 'CRO', 'CIO', 'COO','CAO']
 
 officer_map = {'chief executive officer': 'CEO', 'chief underwriting officer': 'CUO', 'chief financial officer':
-'CFO', 'chief technical officer': 'CTO', 'chief risk officer':'CRO', 
+'CFO', 'chief finance officer':'CFO ', 'chief technical officer': 'CTO', 'chief risk officer':'CRO', 
 'chief information officer': 'CIO', 'chief operations officer':'COO', 'chief operating officer':'COO',
 'chief accounting officer': 'CAO','chief investment officer':'CInvestmentO', 'chief human resources officer': 'CHRO',
 'chief strategy and innovation officer':'CSIO', 'chief audit executive':'CAE',
-'chief administrative officer':'CAdminO', 'chief claims officer':'CCO', 'chief communications officer':'CComsO'
+'chief administrative officer':'CAdminO', 'chief claims officer':'CCO', 'chief communications officer':'CComsO',
+'chief sustainability officer':'CSO'
 }
 
+
 df.drop('simplified_role', axis=1, inplace = True)
-df['simplified_column'] = ""
+df['simplified_role'] = ""
 
 for i, role in enumerate(df['role']):
     simple_r = ""
@@ -33,12 +36,14 @@ for i, role in enumerate(df['role']):
             if j == 'CTO':
                 if 'DIRECTOR' not in df['role'][i]:
                     simple_r = simple_r + " " + j
+            elif 'ASSISTANT' in df['role'][i].upper():
+                simple_r = ""
             else:
                 simple_r = simple_r + " " + j
     for word, acronym in officer_map.items():
         simple_r = simple_r.replace(word.lower(), acronym)
 
-    df['simplified_column'][i] = simple_r
+    df['simplified_role'][i] = simple_r
 
 df.to_excel('TEST.xlsx')
 
